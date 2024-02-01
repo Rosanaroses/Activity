52bae66d0efefb6bcef91f8308fce31ce69e6723
STC <STC@DESKTOP-RLSGPBF>
2024-02-01 14:35:32 +0800

    Add project files.

---
+++ Activity/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        MonthCalendar1 = New MonthCalendar()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(227, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(366, 42)
+        Button1.TabIndex = 0
+        Button1.Text = "Activity 1"
+        Button1.TextImageRelation = TextImageRelation.ImageAboveText
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' MonthCalendar1
+        ' 
+        MonthCalendar1.Location = New Point(18, 152)
+        MonthCalendar1.Name = "MonthCalendar1"
+        MonthCalendar1.TabIndex = 1
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackgroundImage = My.Resources.Resources.e9809d75b0a973d4603b3a37bfd17f9a
+        PictureBox1.BackgroundImageLayout = ImageLayout.Center
+        PictureBox1.Location = New Point(-22, -7)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(875, 458)
+        PictureBox1.TabIndex = 2
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        BackgroundImageLayout = ImageLayout.None
+        ClientSize = New Size(846, 450)
+        Controls.Add(MonthCalendar1)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Activity/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        MessageBox.Show("GoJo")
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
